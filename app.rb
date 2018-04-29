d652fc490b984d9f33f2578c2eefd456d38806fe
Matt Trussell <smtrussell@gmail.com>
2018-04-29 04:15:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,15 +8,15 @@ class App < Sinatra::Base
 
 
   post '/student' do
-    @student = Student.new(params[:student])
-
-    params[:student][:courses].each do |details|
-      Course.new(details)
-    end
-
-    @courses = Course.all
-
-    erb :student
+  @student = Student.new(params[:student])
+ 
+  params[:student][:courses].each do |details|
+    Course.new(details)
   end
+ 
+  @courses = Course.all
+ 
+  erb :student
+end
 
 end
